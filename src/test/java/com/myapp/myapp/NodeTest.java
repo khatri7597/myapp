e9e806c37110a66beef58fe9374a4a5320041be5
hitesh.khatri <hitesh.khatri@MS-Hiteshkhatri.icpl.local>
2019-03-07 13:21:31 +0530

    Dev branch commit

---
+++ src/test/java/com/myapp/myapp/NodeTest.java
@@ -0,0 +1,27 @@
+package com.myapp.myapp;
+
+public class NodeTest {
+	
+	private Integer value;
+	private NodeTest left;
+	private NodeTest right;
+	public Integer getValue() {
+		return value;
+	}
+	public void setValue(Integer value) {
+		this.value = value;
+	}
+	public NodeTest getLeft() {
+		return left;
+	}
+	public void setLeft(NodeTest left) {
+		this.left = left;
+	}
+	public NodeTest getRight() {
+		return right;
+	}
+	public void setRight(NodeTest right) {
+		this.right = right;
+	}
+	
+}
